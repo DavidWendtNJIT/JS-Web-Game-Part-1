dcd93d0784be8d4090d12acb2922a85d1d6e4587
DavidWendtNJIT <djw5383@gmail.com>
2022-03-08 22:54:37 -0500

    used css when I couldn't figure out how the for loop

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+  let element = document.createElement("img");
+  element.src = url;
+  element.style.position = "fixed";
+  element.style.left = left + "px";
+  element.style.bottom = bottom + "px";
+  document.body.append(element);
+  return element
+}
+
+function newItem (url, left, bottom) {
+  let object = newImage(url, left, bottom)
+    object.addEventListener("dblclick" , function () {
+      object.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 950, 400)
+newImage('assets/tree.png', 200, 480)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
